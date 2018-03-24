caae7f8cee398790a3547bff9a63505505d34bf1
drinkinsailor <drinkinsailor@gmaol.com>
2018-03-24 11:40:10 +0200

    asda

--- tad/js/common.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() { 
-   $('.logo').addClass('animated fadeIn');
-   $('.head-text h1').addClass('animated fadeIn');
-   $('.head-text p').addClass('animated fadeIn');
-
-   });
