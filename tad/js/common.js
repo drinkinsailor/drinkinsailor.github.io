93123df11f82b68bdb64cd4384341edb5a902ecf
drinkinsailor <drinkinsailor@gmaol.com>
2018-03-24 10:27:30 +0200

    tad commit

---
+++ tad/js/common.js
@@ -0,0 +1,6 @@
+$(document).ready(function() { 
+   $('.logo').addClass('animated fadeIn');
+   $('.head-text h1').addClass('animated fadeIn');
+   $('.head-text p').addClass('animated fadeIn');
+
+   });
